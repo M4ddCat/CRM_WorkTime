84d6c1578eb0ce4fb4df5c746f0531233b7bf82d
M4ddCat <m4ddcat@gmail.com>
2023-02-28 16:02:39 +0500

    asd

--- WorkTime.Web/Services/UserInformation.cs
+++ WorkTime.Web/Services/UserInformation.cs
@@ -1,4 +1,7 @@
-﻿using WorkTime.Data;
+﻿using Newtonsoft.Json.Linq;
+using System.Globalization;
+using WorkTime.Data;
+using WorkTime.Models;
 
 namespace WorkTime.Web.Services
 {
@@ -12,24 +15,101 @@ namespace WorkTime.Web.Services
             _context = new WorkTimeContext();
         }
 
-        public string GetName()
+        public string GetUserName()
         {
-            string userName = _httpContext.HttpContext?.User.Identity.Name;
-            string userId = _context.AspNetUsers.FirstOrDefault(u => u.UserName == userName)?.Id;
-            var userInfo = _context.AspNetUserInformations.FirstOrDefault(u => u.UserId == userId);
+            AspNetUserInformation? userInfo = _context.AspNetUserInformations.FirstOrDefault(u => u.UserId == GetUId());
+            if (userInfo == null) throw new ArgumentNullException();
             return $"{userInfo.Name} {userInfo.Surname}";
         }
 
-        public string GetName(string id)
+        public string GetUserName(string id)
         {
-            var userInfo = _context.AspNetUserInformations.FirstOrDefault(u => u.UserId == id);
+            AspNetUserInformation? userInfo = _context.AspNetUserInformations.FirstOrDefault(u => u.UserId == id);
+            if (userInfo == null) throw new ArgumentNullException();
             return $"{userInfo.Name} {userInfo.Surname}";
         }
 
-        public string GetMyId()
+        public string GetUserId()
         {
-            string userName = _httpContext.HttpContext?.User.Identity.Name;
-            return _context.AspNetUsers.FirstOrDefault(u => u.UserName == userName)?.Id;
+            return GetUId();
+        }
+
+        public int GetUserCountOfTasks()
+        {
+            return _context.WorkTasks.Where(t => (t.TaskStatusId == 1 || t.TaskStatusId == 2) && t.PerformerId == GetUId()).Count();
+        }
+
+        public string GetUserMounthlyMoney()
+        {
+            string uId = GetUId();
+            DateTime now = DateTime.Now;
+            var mas = _context.WorkTasks
+                .Where(t => t.DateOfCompletion.Value.Month == now.Month
+                && t.DateOfCompletion.Value.Year == now.Year
+                && t.PerformerId == uId
+                && (t.TaskStatusId == 3 || t.TaskStatusId == 4))
+                .Select(t => t.CountOfHours).ToArray();
+            if (mas.Length == 0)
+            {
+                return "0";
+            }
+            double countOfHours = 0;
+            for (int i = 0; i != mas.Length; i++)
+            {
+                countOfHours += mas[i];
+            }
+            double? hourlyWage = _context.AspNetUserInformations.FirstOrDefault(u => u.UserId == uId)?.HourlyWage;
+            if (hourlyWage == null)
+                throw new ArgumentNullException();
+            double money = (double)(countOfHours * hourlyWage);
+            return money.ToString("C", CultureInfo.CreateSpecificCulture("ru-RU"));
+        }
+
+        public MonthTasks[] GetUserTasksByMonths()
+        {
+            string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+            DateTime date = DateTime.Now;
+            int nowMonth = date.Month - 1;
+            int nowYear = date.Year;
+            string uId = GetUId();
+            MonthTasks[] monthTasks = new MonthTasks[6];
+            for (int i = 0; i < 6; i++)
+            {
+                int count = _context.WorkTasks.Where(t => t.DateOfCompletion.Value.Year == nowYear
+                && t.DateOfCompletion.Value.Month == nowMonth + 1 
+                && (t.TaskStatusId == 3 || t.TaskStatusId == 4)
+                && t.PerformerId == uId).Count();
+                monthTasks[i] = new MonthTasks(months[nowMonth], count);
+
+                if (nowMonth == 0)
+                {
+                    nowMonth = 12;
+                    nowYear--;
+                }
+                nowMonth--;
+            }
+            return monthTasks;
+        }
+
+        private string GetUId()
+        {
+            string? uName = _httpContext.HttpContext?.User.Identity?.Name;
+            if (uName == null)
+                throw new ArgumentNullException();
+            string? uId = _context.AspNetUsers.FirstOrDefault(u => u.UserName == uName)?.Id;
+            if (uId == null)
+                throw new ArgumentNullException();
+            return uId;
+        }
+
+        public class MonthTasks
+        {
+            public string month;
+            public int tasks;
+            public MonthTasks(string _month, int _tasks)
+            {
+                month = _month; tasks = _tasks;
+            }
         }
     }
 }
--- WorkTime.Web/wwwroot/js/site.js
+++ WorkTime.Web/wwwroot/js/site.js
@@ -2,8 +2,10 @@
     const element = document.getElementById('sideBar'),
         scrim = document.getElementById('scrim'),
         menu = document.getElementById('sideBarMenu');
+
     
     if (element.classList.contains('hidden')) {
+        document.body.classList.toggle('no-scroll');
         element.classList.toggle('hidden');
         setTimeout(() => {
         scrim.classList.toggle('showed');
@@ -12,6 +14,10 @@
     else {
         scrim.classList.toggle('showed');
         menu.classList.toggle('show');
-        setTimeout(() => { element.classList.toggle('hidden'); }, 200);
+        setTimeout(() => {
+            element.classList.toggle('hidden');
+            document.body.classList.toggle('no-scroll');
+        }, 200);
+        
     }
 }
